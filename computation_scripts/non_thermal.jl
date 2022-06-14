bc1d95ca88bc76a73d472b68eefac6d65b9c0fe3
Aleksandr Rodin <alerodin@gmail.com>
2022-06-14 18:53:48 +0800

    Trajectory loss figure

--- computation_scripts/non_thermal.jl
+++ computation_scripts/non_thermal.jl
@@ -19,7 +19,7 @@ function Δ_numeric(σ_dot, σ0, Φ0, λ, system)
     ρHs = zeros(nChain, n_pts)
     tTraj = ThermalTrajectory(system.ωmax, system.δ, ρHs, nothing)
 
-    res = motion_solver(system, Φ0, λ, α, [σ0], [σ_dot], μ, tTraj, Inf, τ, threads = true)
+    res = motion_solver(system, Φ0, λ, α, [σ0], [σ_dot], μ, tTraj, Inf, τ, threads=true)
 
     σs = res.σs |> vec
     mid_pt_idx = findmin(abs.(σs .- (σ0 + α)))[2]
@@ -30,7 +30,7 @@ end
 # Low speed
 Φ0s = 0.025
 par = [(λ, Φ0) for λ in λs, Φ0 in [Φ0s, -Φ0s]] |> vec
-σ_dots = range(2, 20, length = vPts)
+σ_dots = range(2, 20, length=vPts)
 
 
 if (!isfile("data/non_thermal/Single_Pass_Slow_Φ$(Φ0s)_μ$(μ).jld2"))
@@ -48,7 +48,7 @@ end
 # High speed
 Φ0s = 2.0
 par = [(λ, Φ0) for λ in λs, Φ0 in [Φ0s, -Φ0s]] |> vec
-σ_dots = range(20, 350, length = vPts)
+σ_dots = range(20, 350, length=vPts)
 
 if (!isfile("data/non_thermal/Single_Pass_Fast_Φ$(Φ0s)_μ$(μ).jld2"))
 
@@ -62,251 +62,41 @@ if (!isfile("data/non_thermal/Single_Pass_Fast_Φ$(Φ0s)_μ$(μ).jld2"))
     save_object("data/non_thermal/Single_Pass_Fast_Φ$(Φ0s)_μ$(μ).jld2", fast_res)
 end
 
+## FULL TRAJECTORY
+system = load_object("precomputed/systems/System_ωmax10_d60_l300.jld2")
+d = 60
+τ = 250                             # Simulation time
+δ = system.δ                        # Time step
+α = 10                              # Distance between chain atoms
+μ = 1
+σ0 = [55]
 
+n_pts = τ / δ |> floor |> Int
+nChain = 250
+ρHs = zeros(nChain, n_pts)
+tTraj = ThermalTrajectory(system.ωmax, system.δ, ρHs, nothing)
+mem = Inf
 
+params = [
+    (1, 1 / 2, [30]), (-1, 1 / 2, [30]),
+]
 
-# d = 60
-# σ_dots = range(20, 350, length = vPts)
+println("Starting Calculations")
+for param in params
+    println(param)
+    Φ0 = param[1]
+    λ = param[2]
+    σdot0 = param[3]
+    if (
+        !isfile(
+            "data/non_thermal/Single_σ0$(σ0)_σdot0$(σdot0)_Mem$(mem)_λ$(λ)_Φ$(Φ0)_μ$(μ)_d$(d)_ΩT$(nothing)_τ$(τ).jld2",
+        )
+    )
 
-# fig = Figure(resolution = (1200, 800), font = "CMU Serif", fontsize = 36)
-# ax1 = Axis(
-#     fig[1, 1],
-#     xlabel = L"\dot{\sigma}_0",
-#     ylabel = L"\Delta",
-#     yscale = log10,
-#     # xscale = log10,
-#     # yminorticksvisible = true,
-#     # yminorgridvisible = true,
-#     # yminorticks = IntervalsBetween(10),
-#     # xminorticksvisible = true,
-#     # xminorgridvisible = true,
-#     # xminorticks = IntervalsBetween(10),
-# )
-
-
-# for ii = 1:length(λs)
-
-#     λ = λs[ii]
-#     res_att = [Δ_numeric(x, σ0, -Φ0, λ, system_fast) for x in σ_dots]
-#     res_rep = [Δ_numeric(x, σ0, Φ0, λ, system_fast) for x in σ_dots]
-#     analytic = Δ_analytic.(σ_dots, Φ0, λ, system_fast.ωmax)
-
-#     scatter!(ax1, (σ_dots), (res_att), color = colors[ii], marker = :hline, markersize = 20)
-#     scatter!(ax1, (σ_dots), (res_rep), color = colors[ii], marker = :cross, markersize = 20)
-#     lines!(
-#         ax1,
-#         (σ_dots),
-#         (analytic),
-#         color = colors[ii],
-#         linewidth = 4,
-#         label = L"\lambda = %$λ",
-#     )
-# end
-# axislegend(ax1, position = :rt)
-# fig
-
-# colors = [my_vermillion, my_orange, my_green, my_sky, my_blue]
-
-
-# function Δ_analytic(v, Φ, λ, Ω)
-#     z = (2 * π * λ / v)^2
-#     return (
-#         4 * π^3 * Φ^2 / v^2 *
-#         z *
-#         exp(-z * (Ω^2 + 1) / 2) *
-#         (
-#             besseli(0, z * (Ω^2 - 1) / 2) +
-#             (Ω^2 - 1) / 2 * (besseli(0, z * (Ω^2 - 1) / 2) - besseli(1, z * (Ω^2 - 1) / 2))
-#         )
-#     )
-# end
-
-
-# fig = Figure(resolution = (1200, 800), font = "CMU Serif", fontsize = 36)
-# ax1 = Axis(
-#     fig[1, 1],
-#     xlabel = L"\dot{\sigma}_0",
-#     ylabel = L"\Delta",
-#     yscale = log10,
-#     # xscale = log10,
-#     # yminorticksvisible = true,
-#     # yminorgridvisible = true,
-#     # yminorticks = IntervalsBetween(10),
-#     # xminorticksvisible = true,
-#     # xminorgridvisible = true,
-#     # xminorticks = IntervalsBetween(10),
-# )
-
-
-# for ii = 1:length(λs)
-
-#     analytic = Δ_analytic.(σ_dots, Φ0, λ, system_slow.ωmax)
-
-#     scatter!(ax1, (σ_dots), (res_att), color = colors[ii], marker = :hline, markersize = 20)
-#     scatter!(ax1, (σ_dots), (res_rep), color = colors[ii], marker = :cross, markersize = 20)
-#     lines!(
-#         ax1,
-#         (σ_dots),
-#         (analytic),
-#         color = colors[ii],
-#         linewidth = 4,
-#         label = L"\lambda = %$λ",
-#     )
-# end
-# axislegend(ax1, position = :rb)
-# save("Slow_dissipation.pdf", fig)
-
-
-# # # GENERAL EXAMPLE
-# system = load_object("precomputed/systems/System_ωmax10_d60_l200.jld2")
-# d = 60
-# τ = 200                             # Simulation time
-# δ = system.δ                        # Time step
-# α = 10                              # Distance between chain atoms
-# μ = 1
-
-# n_pts = τ / δ |> floor |> Int
-# nChain = 200
-# ρHs = zeros(nChain, n_pts)
-# tTraj = ThermalTrajectory(system.ωmax, system.δ, ρHs, nothing)
-# σdot0 = [25]
-# mem = Inf
-
-# σ0 = [55]
-# Φ0s = [1 / 2, -1 / 2, -1, 1]
-# # Φ0 = [1 / 2, 1, 2, -1 / 2, -1, -2]
-# λs = [1 / 2, 1]
-# params = [(f, l) for f in Φ0s, l in λs] |> vec
-# println("Starting Calculations")
-# for param in params
-#     println(param)
-#     Φ0 = param[1]
-#     λ = param[2]
-#     if (
-#         !isfile(
-#             "data/non_thermal/Single_σ0$(σ0)_σdot0$(σdot0)_Mem$(mem)_λ$(λ)_Φ$(Φ0)_μ$(μ)_d$(d)_ΩT$(nothing)_τ$(τ).jld2",
-#         )
-#     )
-
-#         res = motion_solver(system, Φ0, λ, α, σ0, σdot0, μ, tTraj, mem, τ)
-#         save_object(
-#             "data/non_thermal/Single_σ0$(σ0)_σdot0$(σdot0)_Mem$(mem)_λ$(λ)_Φ$(Φ0)_μ$(μ)_d$(d)_ΩT$(nothing)_τ$(τ).jld2",
-#             res,
-#         )
-#     end
-# end
-# @showprogress pmap(params) do param
-#     Φ0 = param[1]
-#     λ = param[2]
-#     if (
-#         !isfile(
-#             "data/non_thermal/Single_σ0$(σ0)_σdot0$(σdot0)_Mem$(mem)_λ$(λ)_Φ$(Φ0)_μ$(μ)_d$(d)_ΩT$(nothing)_τ$(τ).jld2",
-#         )
-#     )
-
-#         res = motion_solver(system, Φ0, λ, α, σ0, σdot0, μ, tTraj, mem, τ)
-#         save_object(
-#             "data/non_thermal/Single_σ0$(σ0)_σdot0$(σdot0)_Mem$(mem)_λ$(λ)_Φ$(Φ0)_μ$(μ)_d$(d)_ΩT$(nothing)_τ$(τ).jld2",
-#             res,
-#         )
-#     end
-# end
-
-
-# ## Burst Energy Loss
-# system = load_object("precomputed/systems/System_ωmax10_d3000_l10.jld2")
-
-# d = 3000
-# δ = system.δ
-# α = 10
-# Φ0 = 1/2
-# σ0 = 45
-# nChain = 10
-# vPts = 100
-# σ_dots = range(20, 350, length = vPts)
-# Δs = zeros(vPts)
-
-
-# @showprogress for ii = 1:vPts
-#     σ_dot = σ_dots[ii]
-#     τ = 1.1 * (α / σ_dot)
-#     n_pts = τ / δ |> floor |> Int
-#     ρHs = zeros(nChain, n_pts)
-#     tTraj = ThermalTrajectory(system.ωmax, system.δ, ρHs, nothing)
-
-#     res = motion_solver(system, Φ0, 1/4, α, [σ0], [σ_dot], 1, tTraj, Inf, τ)
-#     σs = res.σs |> vec
-#     mid_pt_idx = findmin(abs.(σs .- (σ0 + α)))[2]
-#     v_final = (σs[mid_pt_idx] - σs[mid_pt_idx-1]) / δ
-#     Δs[ii] = μ / 2 / (2 * π)^2 * (σ_dot^2 - v_final^2)
-# end
-
-# λ = 1/4
-# Φ = 1/2
-# function en_loss(v)
-#     z = (2 * π * λ / v)^2
-#     return (
-#         4 * π^3 * Φ^2 / v^2 *
-#         z *
-#         exp(-z * (Ω^2 + 1) / 2) *
-#         (
-#             besseli(0, z * (Ω^2 - 1) / 2) +
-#             (Ω^2 - 1) / 2 * (besseli(0, z * (Ω^2 - 1) / 2) - besseli(1, z * (Ω^2 - 1) / 2))
-#         )
-#     )
-# end
-# analytic = en_loss.(σ_dots)
-
-# fig = Figure(resolution = (1200, 800), font = "CMU Serif", fontsize = 36)
-# ax1 = Axis(fig[1, 1], xlabel = L"\tau", ylabel = L"\sigma")
-
-# # lines!(ax1,log.(σ_dots), log.(Δs))
-# # # lines!(ax1,log.(σ_dots), log.(Δs_2))
-# # lines!(ax1, log.(σ_dots), log.(analytic))
-# # Δs- Δs_2
-
-
-# lines!(ax1,(σ_dots), (Δs))
-# # lines!(ax1,(σ_dots), (Δs_2))
-# lines!(ax1, (σ_dots), (analytic))
-# fig
-
-
-# μs = [1 / 5, 1, 5]
-# λs = [1 / 8, 1 / 4, 1 / 2, 1, 2]
-
-# σ_dot = 25
-
-
-# res = motion_solver(system, Φ0, λ, α, σ0, σdot0, μ, tTraj, Inf, τ)
-# σs = res.σs |> vec
-# v = (σs[2:end] - σs[1:end-1]) / (res.τs[2] - res.τs[1])
-# scatter((res.τs)[2:end], v)
-
-
-# mid_pt_idx = findmin(abs.(σs .- (σ0 + α)))[2]
-# v_final = (σs[mid_pt_idx] - σs[mid_pt_idx-1]) / δ
-# Δ_en = μ / 2 / (2 * π)^2 * (σ_dot^2 - v_final^2)
-# μ
-# σs[mid_pt_idx]
-
-
-# # system = load_object("precomputed/systems/System_ωmax10_d3000_l10.jld2")
-
-# # d = 3000
-# # τ = .4                            # Simulation time
-# # δ = system.δ                        # Time step
-# # a = 10                               # Distance between chain atoms
-# # n_pts = τ / δ |> floor |> Int
-# # nChain = 10
-# # ρHs = zeros(nChain, n_pts)
-# # tTraj = ThermalTrajectory(system.ωmax, system.δ, ρHs, nothing)
-# # λ = 1;
-# # Φ = 1;
-# # μ = 25/1;
-# # @time res = motion_solver(system, Φ, λ, a, [25], [25], μ, tTraj, Inf, τ)
-# # s = (res.σs |> vec)
-# # v = (s[2:end] - s[1:end-1]) / (res.τs[2] - res.τs[1])
-# # scatter((res.τs)[2:end], v)
-# # kin_en = μ * v.^ 2 / 2 / (2 * π)^2
-# # kin_en[end] - kin_en[1]
+        res = motion_solver(system, Φ0, λ, α, σ0, σdot0, μ, tTraj, mem, τ, threads=true)
+        save_object(
+            "data/non_thermal/Single_σ0$(σ0)_σdot0$(σdot0)_Mem$(mem)_λ$(λ)_Φ$(Φ0)_μ$(μ)_d$(d)_ΩT$(nothing)_τ$(τ).jld2",
+            res,
+        )
+    end
+end
--- plotting/single_non_thermal.jl
+++ plotting/single_non_thermal.jl
@@ -5,11 +5,11 @@ slow = load_object("data/non_thermal/Single_Pass_Slow_Φ0.025_μ1.jld2")
 fast = load_object("data/non_thermal/Single_Pass_Fast_Φ2.0_μ1.jld2")
 colors = [my_vermillion, my_orange, my_green, my_sky]
 
-fig = Figure(resolution = (1200, 1600), font = "CMU Serif", fontsize = 36)
-ax1 = Axis(fig[1, 1], xlabel = L"\dot{\sigma}_0", ylabel = L"\Delta", yscale = log10)
-ax2 = Axis(fig[2, 1], xlabel = L"\dot{\sigma}_0", ylabel = L"\Delta", yscale = log10)
-slow_keys = sort(collect(keys(slow)), by = x -> x[2])
-fast_keys = sort(collect(keys(fast)), by = x -> x[2])
+fig = Figure(resolution=(1200, 1600), font="CMU Serif", fontsize=36)
+ax1 = Axis(fig[1, 1], xlabel=L"\dot{\sigma}_0", ylabel=L"\Delta", yscale=log10)
+ax2 = Axis(fig[2, 1], xlabel=L"\dot{\sigma}_0", ylabel=L"\Delta", yscale=log10)
+slow_keys = sort(collect(keys(slow)), by=x -> x[2])
+fast_keys = sort(collect(keys(fast)), by=x -> x[2])
 
 for ii = 1:length(slow_keys)
     k = slow_keys[ii]
@@ -18,9 +18,9 @@ for ii = 1:length(slow_keys)
         ax1,
         r[1],
         r[2],
-        color = colors[(ii+1)÷2],
-        marker = k[1] > 0 ? :cross : :hline,
-        markersize = 20,
+        color=colors[(ii+1)÷2],
+        marker=k[1] > 0 ? :cross : :hline,
+        markersize=20,
     )
 
     if k[1] > 0
@@ -29,9 +29,9 @@ for ii = 1:length(slow_keys)
             ax1,
             r[1],
             analytic,
-            color = colors[(ii+1)÷2],
-            linewidth = 4,
-            label = L"\lambda = %$(k[2])",
+            color=colors[(ii+1)÷2],
+            linewidth=4,
+            label=L"\lambda = %$(k[2])",
         )
     end
 
@@ -41,9 +41,9 @@ for ii = 1:length(slow_keys)
         ax2,
         r[1],
         r[2],
-        color = colors[(ii+1)÷2],
-        marker = k[1] > 0 ? :cross : :hline,
-        markersize = 20,
+        color=colors[(ii+1)÷2],
+        marker=k[1] > 0 ? :cross : :hline,
+        markersize=20,
     )
 
     if k[1] > 0
@@ -52,156 +52,125 @@ for ii = 1:length(slow_keys)
             ax2,
             r[1],
             analytic,
-            color = colors[(ii+1)÷2],
-            linewidth = 4,
-            label = L"\lambda = %$(k[2])",
+            color=colors[(ii+1)÷2],
+            linewidth=4,
+            label=L"\lambda = %$(k[2])",
         )
     end
 
 
 end
-axislegend(ax1, position = :rb)
-axislegend(ax2, position = :rt)
+axislegend(ax1, position=:rb)
+axislegend(ax2, position=:rt)
 
 save("Dissipation.pdf", fig)
 
 
 ## TRAJECTORY ENERGY LOSS
 
-fig = Figure(resolution = (1200, 800), font = "CMU Serif", fontsize = 36)
-ax1 = Axis(fig[1, 1], xlabel = L"\dot{\sigma}_0", ylabel = L"\Delta")
+fig = Figure(resolution=(1200, 800), font="CMU Serif", fontsize=36)
+ax1 = Axis(fig[1, 1], xlabel=L"\dot{\sigma}_0", ylabel=L"\Delta")
+
+# REPULSIVE
 data = load_object(
-    "data/non_thermal/Single_σ0[55]_σdot0[20]_MemInf_λ0.5_Φ0.5_μ1_d60_ΩTnothing_τ100.jld2",
+    "data/non_thermal/Single_σ0[55]_σdot0[30]_MemInf_λ0.5_Φ1_μ1_d60_ΩTnothing_τ250.jld2",
 )
 
 res_numeric = Δ_traj(data)
-res_analytic = Δ_analytic.(res_numeric[1], data.Φ, data.λ, data.ωmax)
 
-scatter!(ax1, res_numeric[1], res_numeric[2])
-lines!(ax1, res_numeric[1], res_analytic)
+scatter!(ax1, res_numeric[1], res_numeric[2], marker=:cross, markersize=20, color=my_vermillion)
+
+# ATTRACTIVE
+data = load_object(
+    "data/non_thermal/Single_σ0[55]_σdot0[30]_MemInf_λ0.5_Φ-1_μ1_d60_ΩTnothing_τ250.jld2",
+)
+
+res_numeric = Δ_traj(data)
+vs = range(res_numeric[1][1], res_numeric[1][end], length=1000)
+res_analytic = Δ_analytic.(vs, data.Φ, data.λ, data.ωmax)
+
+scatter!(ax1, res_numeric[1], res_numeric[2], marker=:hline, markersize=20, color=my_blue)
+lines!(ax1, vs, res_analytic,
+    linewidth=4, color=my_black
+)
 
 fig
+save("Trajectory_Loss.pdf", fig)
 
 
-# step_size = 20
-# ## General Example
+## General Example
+step_size = 20
 
-# fig = Figure(resolution = (1200, 1600), font = "CMU Serif", fontsize = 36)
-# ax1 = Axis(fig[1, 1], xlabel = L"\tau", ylabel = L"\sigma")
-# ax2 = Axis(fig[2, 1], xlabel = L"\tau", ylabel = L"\sigma")
+fig = Figure(resolution=(1200, 1600), font="CMU Serif", fontsize=36)
+ax1 = Axis(fig[1, 1], xlabel=L"\tau", ylabel=L"\sigma")
+ax2 = Axis(fig[2, 1], xlabel=L"\tau", ylabel=L"\sigma")
 
-# # REPULSIVE
+# REPULSIVE
 
-# data = load_object(
-#     "data/non_thermal/Single_σ0[55]_σdot0[25]_MemInf_λ0.5_Φ1.0_μ1_d60_ΩTnothing_τ200.jld2",
-# )
-# δ = data.τs[2] - data.τs[1]
-# rr = reduce(hcat, [data.ρs[ii, :] .- ii * data.α for ii = 1:size(data.ρs)[1]])
-# mx = maximum(abs.(rr)) / 5
-# hm = heatmap!(
-#     ax1,
-#     data.τs[1:step_size:end],
-#     collect(1:size(data.ρs)[1]) .* data.α,
-#     rr[1:step_size:end, :],
-#     colormap = :RdBu,
-#     colorrange = (-mx, mx),
-# )
-# lines!(ax1, data.τs, [x[1] for x in data.σs] |> vec, color = my_black, linewidth = 5)
-# xlims!(ax1, (0, 100))
-# ylims!(ax1, (0, 1500))
+data = load_object(
+    "data/non_thermal/Single_σ0[55]_σdot0[30]_MemInf_λ0.5_Φ1_μ1_d60_ΩTnothing_τ250.jld2",
+)
+δ = data.τs[2] - data.τs[1]
+τ_max = 105
+idx = findall(data.τs .< τ_max)
+τs = data.τs[idx]
+rr = reduce(hcat, [data.ρs[ii, idx] .- ii * data.α for ii = 1:size(data.ρs)[1]])
+# mx = maximum(abs.(rr)) / 1
+mx = 0.03
+hm = heatmap!(
+    ax1,
+    τs[1:step_size:end],
+    collect(1:size(data.ρs)[1]) .* data.α,
+    rr[1:step_size:end, :],
+    colormap=:RdBu,
+    colorrange=(-mx, mx),
+)
+lines!(ax1, data.τs, [x[1] for x in data.σs] |> vec, color=my_black, linewidth=5)
+xlims!(ax1, (0, 105))
+ylims!(ax1, (0, 2500))
 
-# lines!(
-#     ax1,
-#     0:0.1:6,
-#     π * 10 * 9 * (0:0.1:6) .+ data.σs[1][1],
-#     color = my_black,
-#     linewidth = 4,
-#     linestyle = :dash,
-# )
-# Colorbar(fig[1, 2], hm; label = L"\Delta\rho", width = 15, ticksize = 15, tickalign = 1)
-# # ATTRACTIVE
+lines!(
+    ax1,
+    0:0.1:10,
+    π * 10 * 9 * (0:0.1:10) .+ data.σs[1][1],
+    color=my_black,
+    linewidth=4,
+    linestyle=:dash,
+)
+Colorbar(fig[1, 2], hm; label=L"\Delta\rho", width=15, ticksize=15, tickalign=1)
+# ATTRACTIVE
 
-# data = load_object(
-#     "data/non_thermal/Single_σ0[55]_σdot0[25]_MemInf_λ0.5_Φ-1.0_μ1_d60_ΩTnothing_τ200.jld2",
-# )
-# δ = data.τs[2] - data.τs[1]
-# rr = reduce(hcat, [data.ρs[ii, :] .- ii * data.α for ii = 1:size(data.ρs)[1]])
-# mx = maximum(abs.(rr)) / 5
-# hm = heatmap!(
-#     ax2,
-#     data.τs[1:step_size:end],
-#     collect(1:size(data.ρs)[1]) .* data.α,
-#     rr[1:step_size:end, :],
-#     colormap = :RdBu,
-#     colorrange = (-mx, mx),
-# )
-# lines!(ax2, data.τs, [x[1] for x in data.σs] |> vec, color = my_black, linewidth = 5)
-# xlims!(ax2, (0, 100))
-# ylims!(ax2, (0, 1500))
+data = load_object(
+    "data/non_thermal/Single_σ0[55]_σdot0[30]_MemInf_λ0.5_Φ-1_μ1_d60_ΩTnothing_τ250.jld2",
+)
+δ = data.τs[2] - data.τs[1]
+τ_max = 105
+idx = findall(data.τs .< τ_max)
+τs = data.τs[idx]
+rr = reduce(hcat, [data.ρs[ii, idx] .- ii * data.α for ii = 1:size(data.ρs)[1]])
+mx = 0.03
+# mx = maximum(abs.(rr)) / 3
+hm = heatmap!(
+    ax2,
+    τs[1:step_size:end],
+    collect(1:size(data.ρs)[1]) .* data.α,
+    rr[1:step_size:end, :],
+    colormap=:RdBu,
+    colorrange=(-mx, mx),
+)
+lines!(ax2, data.τs, [x[1] for x in data.σs] |> vec, color=my_black, linewidth=5)
+xlims!(ax2, (0, 105))
+ylims!(ax2, (0, 2500))
 
-# lines!(
-#     ax2,
-#     0:0.1:6,
-#     π * 10 * 9 * (0:0.1:6) .+ data.σs[1][1],
-#     color = my_black,
-#     linewidth = 4,
-#     linestyle = :dash,
-# )
-# Colorbar(fig[2, 2], hm; label = L"\Delta\rho", width = 15, ticksize = 15, tickalign = 1)
-# fig
-# save("General_Example.pdf", fig)
-
-
-
-# lines(ts[idx], σ_dots[idx])
-
-
-
-
-# 0.5 * (5 / 2 / pi)^2
-
-
-# lines!(ax1, data.τs, [x[1] for x in data.σs] |> vec, color = my_black, linewidth = 5)
-# # xlims!(ax1, (0, 80))
-# ylims!(ax1, (0, 1500))
-
-# lines!(
-#     ax1,
-#     0:0.1:6,
-#     π * 10 * 9 * (0:0.1:6) .+ data.σs[1][1],
-#     color = my_black,
-#     linewidth = 4,
-#     linestyle = :dash,
-# )
-# Colorbar(fig[1, 2], hm; label = L"\Delta\rho", width = 15, ticksize = 15, tickalign = 1)
-# # ATTRACTIVE
-
-# data = load_object(
-#     "data/non_thermal/Single_σ0[55]_σdot0[20]_MemInf_λ1.0_Φ-1.0_μ1_d60_ΩTnothing_τ120.jld2",
-# )
-# δ = data.τs[2] - data.τs[1]
-# rr = reduce(hcat, [data.ρs[ii, :] .- ii * data.α for ii = 1:size(data.ρs)[1]])
-# mx = maximum(abs.(rr))
-# hm = heatmap!(
-#     ax2,
-#     data.τs[1:step_size:end],
-#     collect(1:size(data.ρs)[1]) .* data.α,
-#     rr[1:step_size:end, :],
-#     colormap = :RdBu,
-#     colorrange = (-mx, mx),
-# )
-# lines!(ax2, data.τs, [x[1] for x in data.σs] |> vec, color = my_black, linewidth = 5)
-# # xlims!(ax2, (0, 80))
-# ylims!(ax2, (0, 1500))
-
-# lines!(
-#     ax2,
-#     0:0.1:6,
-#     π * 10 * 9 * (0:0.1:6) .+ data.σs[1][1],
-#     color = my_black,
-#     linewidth = 4,
-#     linestyle = :dash,
-# )
-# Colorbar(fig[2, 2], hm; label = L"\Delta\rho", width = 15, ticksize = 15, tickalign = 1)
-# fig
+lines!(
+    ax2,
+    0:0.1:10,
+    π * 10 * 9 * (0:0.1:10) .+ data.σs[1][1],
+    color=my_black,
+    linewidth=4,
+    linestyle=:dash,
+)
+Colorbar(fig[2, 2], hm; label=L"\Delta\rho", width=15, ticksize=15, tickalign=1)
+fig
+save("General_Example.pdf", fig)
 
